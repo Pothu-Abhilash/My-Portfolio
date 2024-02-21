2709bcb96b33b39782d871de9f4f19507e8992b1
Pothu-Abhilash <abhipothu2000@gmail.com>
2024-02-21 13:20:48 +0530

    updated sticky nav

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const dynamicText = document.querySelector("h4 span");
+const words = ["Full Stack Developer"];
+
+let wordIdx = 0;
+let charIdx = 1;
+let isDeleting = false;
+
+const typeEffect = () => {
+  const currentWord = words[wordIdx];
+  const currentchar = currentWord.substring(0, charIdx);
+  dynamicText.textContent = currentchar;
+
+  if (!isDeleting && charIdx < currentWord.length) {
+    charIdx++;
+    setTimeout(typeEffect, 200);
+  } else if (isDeleting && charIdx > 0) {
+    charIdx--;
+    setTimeout(typeEffect, 100);
+  } else {
+    isDeleting = !isDeleting;
+    setTimeout(typeEffect, 1200);
   }
+};
+typeEffect();
+
+window.addEventListener("scroll", function() {
+  let header = document.querySelector("nav");
+  header.classList.toggle("sticky",window.scrollY > 0);
+})
+
+
